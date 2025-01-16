d708d786eabced6d5c436e43bd3d4e2be0c8d9d3
Z0rr06 <162941822+Z0rr06@users.noreply.github.com>
2025-01-16 16:50:04 -0500

    Sublimart
    aaa

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// Función para aplicar el modo oscuro si está guardado en localStorage
+function aplicarModoOscuro() {
+    if (localStorage.getItem('modoOscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+        document.getElementById('icono').classList.remove('fa-moon');
+        document.getElementById('icono').classList.add('fa-sun');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.getElementById('icono').classList.remove('fa-sun');
+        document.getElementById('icono').classList.add('fa-moon');
+    }
+}
+
+// Aplicar el modo oscuro al cargar la página
+document.addEventListener('DOMContentLoaded', (event) => {
+    aplicarModoOscuro();
+});
+
+document.getElementById('miBoton').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    
+    const icon = this.querySelector('i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+        localStorage.setItem('modoOscuro', 'true');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+        localStorage.setItem('modoOscuro', 'false');
+    }
+});
